96d1467676cd4a65196dfcf10417073cca255e11
Laia <laia@users.noreply.github.com>
2022-05-29 15:55:40 +0200

    "Added Google and RADIUS authentication"

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const logger = require('morgan')
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
 const jwt = require('jsonwebtoken')
 const jwtSecret = require('crypto').randomBytes(16) // 16*8= 256 random bits // It's a random secret to check the HMAC-SHA256 signature of every JWT
 const cookieParser = require('cookie-parser')
 const fortune = require('fortune-teller')
-const JwtStrategy = require('passport-jwt').Strategy;
 const scryptPbkdf = require('scrypt-pbkdf')
 const fs = require('fs')
 const app = express()
@@ -21,11 +19,17 @@ const httpsOptions = {
   cert: tlsServerCrt
 };
 const server = https.createServer(httpsOptions, app);
+const JwtStrategy = require('passport-jwt').Strategy;
+const LocalStrategy = require('passport-local').Strategy
 const gitHubStrategy = require('passport-github2').Strategy
-
+const googleStrategy = require('passport-google-oidc').Strategy
+const radiusStrategy = require('passport-local').Strategy
+const radiusClient = require('radclient')
 const secretConfig = require('./config')
 const GITHUB_CLIENT_ID = secretConfig.GITHUB_ID
 const GITHUB_CLIENT_SECRET = secretConfig.GITHUB_SECRET
+const GOOGLE_CLIENT_ID = secretConfig.GOOGLE_ID
+const GOOGLE_CLIENT_SECRET = secretConfig.GOOGLE_SECRET
 
 const cookieExtractor = req => {
   let jwtCookie = null
@@ -43,18 +47,19 @@ jwtOptions.secretOrKey = jwtSecret;
 jwtOptions.issuer = 'localhost:3000';
 jwtOptions.audience = 'localhost:3000';
 
+
 app.use(bodyParser.urlencoded({ extended: true })) // needed to retrieve html form fields
 app.use(bodyParser.json()) // github
 app.use(cookieParser())
 app.use(logger('dev'))
-app.use(session({ 
-  secret: 'example secret', 
-  resave: false, 
-  saveUninitialized: false, 
-  }))
+app.use(session({
+  secret: 'example secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 app.use(passport.initialize()) // we load the passport auth middleware to our express application. It should be loaded before any route.
 app.use(passport.session())
-
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500).send('Something broke!')
@@ -75,7 +80,7 @@ passport.use('local', new LocalStrategy(
     passwordField: 'password', // it MUST match the name of the input field for the password in the login HTML formulary
     session: false // we will store a JWT in the cookie with all the required session data. Our server does not need to keep a session, it's stateless
   },
-  function (username, password, done) {
+  async function (username, password, done) {
     validateUserPasswd(username, password).then(res => { // cridar una funció async
       if (res) {
         const user = {
@@ -85,6 +90,35 @@ passport.use('local', new LocalStrategy(
         return done(null, user) // the first argument for done is the error, if any. In our case no error so that null. The object user will be added by the passport middleware to req.user and this will be available there for the next middleware and/or the route handler
       }
       return done(null, false) // in passport returning false as the user object means that the authentication process failed.
+    })
+  }
+))
+
+passport.use('radius', new radiusStrategy(
+  {
+    usernameField: 'rUsername',
+    passwordField: 'rPassword',
+    session: false
+  },
+  function (username, password, done) {
+    const packet = {
+      code: 'Access-Request',
+      secret: 'hello1234',
+      identifier: 123,
+      attributes: [
+        ['User-Name', username],
+        ['User-Password', password]
+      ]
+    };
+    const options = {
+      host: '10.0.2.10',
+      port: '1812',
+      timeout: 2000,
+      retries: 3
+    }
+    //return done(null, packet, options)
+    radiusClient(packet, options, (err, res) => {
+      return done(null, res)
     })
   }
 ))
@@ -103,10 +137,11 @@ passport.use(new gitHubStrategy({
   callbackURL: 'https://10.0.2.5/logingithub/callback'
 },
   function (accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {     
+    process.nextTick(function () {
       return done(null, profile)
     })
-  }))
+  }
+))
 
 // To avoid Error: Failed to serialize user into session
 passport.serializeUser(function (user, done) {
@@ -116,6 +151,20 @@ passport.serializeUser(function (user, done) {
 passport.deserializeUser(function (user, done) {
   done(null, user);
 });
+
+/**
+ * PASSPORT FOR GOOGLE
+ */
+
+passport.use(new googleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: 'https://localhost/callback',
+  scope: ['profile'],
+  session: false
+}, function (issuer, profile, done) { console.log(profile); done(null, { username: profile.displayName, provider: 'Google' }) }))
+
+
 
 /**
  * JWT AUTHENTICATION FUNCTIONS
@@ -169,15 +218,15 @@ async function validateUserPasswd(_username, _plainPasswd) {
  * GET ENDPOINTS
  */
 
-app.get('/', passport.authenticate('jwt', {failureRedirect: '/login'}), 
-function(req,res) {
-  // let decodedJWT = jwt.verify(req.cookies['cookie_access_token'], jwtSecret)
-  // let username = decodedJWT.sub
-  // res.json({
-  //   username: username
-  // })
-  res.send(fortune.fortune())
-})
+app.get('/', passport.authenticate('jwt', { failureRedirect: '/login' }),
+  function (req, res) {
+    // let decodedJWT = jwt.verify(req.cookies['cookie_access_token'], jwtSecret)
+    // let username = decodedJWT.sub
+    // res.json({
+    //   username: username
+    // })
+    res.send(fortune.fortune())
+  })
 
 app.get('/login',
   (req, res) => {
@@ -207,7 +256,7 @@ app.get('/logingithub',
 );
 
 app.get('/logingithub/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+  passport.authenticate('github', { failureRedirect: '/login', session: false }),
   function (req, res) {
     // This is what ends up in our JWT
     const jwtClaims = {
@@ -222,14 +271,36 @@ app.get('/logingithub/callback',
     const token = jwt.sign(jwtClaims, jwtSecret)
 
     // Just for testing, send the JWT directly to the browser. Later on we should send the token inside a cookie.
-    //res.json(token)
+    // res.json(token)
 
     res
-      .cookie("cookie_access_token", token, {httpOnly: true, secure: true}) 
+      .cookie("cookie_access_token", token, { httpOnly: true, secure: true })
       //.redirect('/')
       .json({ message: "Logged in successfully!" });
   }
 );
+
+app.get('/logingoogle', passport.authenticate('google', { failureRedirect: '/login', session: false }))
+
+app.get('/callback',
+  passport.authenticate('google', { failureRedirect: '/login', session: false }),
+  (req, res) => {
+    const jwtClaims = {
+      sub: req.user.username,
+      iss: 'localhost:3000',
+      aud: 'localhost:3000',
+      exp: Math.floor(Date.now() / 1000) + 604800, // 1 week (7×24×60×60=604800s) from now
+      role: 'user' // just to show a private JWT field
+    }
+
+    // generate a signed json web token. By default the signing algorithm is HS256 (HMAC-SHA256), i.e. we will 'sign' with a symmetric secret
+    const token = jwt.sign(jwtClaims, jwtSecret)
+    res
+      .cookie("cookie_access_token", token, { httpOnly: true, secure: true })
+      //.redirect('/')
+    .json({ message: "Logged in successfully!" });
+  }
+)
 
 /**
  * POST ENDPOINTS
@@ -258,11 +329,31 @@ app.post('/login',
     //res.json(token)
 
     res
-      .cookie("cookie_access_token", token, {httpOnly: true, secure: true}) // cookie_access_token = name of the cookie
+      .cookie("cookie_access_token", token, { httpOnly: true, secure: true }) // cookie_access_token = name of the cookie
       .status(200)
       .json({ message: 'Logged in successfully!' });
 
     // And let us log a link to the jwt.iot debugger, for easy checking/verifying:
+    console.log(`Token sent. Debug at https://jwt.io/?value=${token}`)
+    console.log(`Token secret (for verifying the signature): ${jwtSecret.toString('hex')}`)
+  }
+)
+
+app.post('/loginradius',
+  passport.authenticate('radius', { failureRedirect: '/login', session: false }),
+  (req, res) => { 
+    const jwtClaims = {
+      sub: req.user.rUsername,
+      iss: 'localhost:3000',
+      aud: 'localhost:3000',
+      exp: Math.floor(Date.now() / 1000) + 604800, // 1 week (7×24×60×60=604800s) from now
+      role: 'user' // just to show a private JWT field
+    }
+    const token = jwt.sign(jwtClaims, jwtSecret)
+    res
+      .cookie("cookie_access_token", token, { httpOnly: true, secure: true }) // cookie_access_token = name of the cookie
+      .status(200)
+      .json({ message: 'Logged in successfully!' });
     console.log(`Token sent. Debug at https://jwt.io/?value=${token}`)
     console.log(`Token secret (for verifying the signature): ${jwtSecret.toString('hex')}`)
   }
@@ -283,5 +374,5 @@ function onListening() {
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   console.log('Listening on ' + bind);
-  console.log('https://10.0.2.5/login')
+  console.log('https://localhost/login')
 }
